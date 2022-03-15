7b848d170972d9ecbba6757a633fa705325fe5ea
vagrant <zachary.sluder@gmail.com>
2022-03-15 20:48:10 +0000

    changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,7 +21,7 @@
     <body>
         <nav class="navbar navbar-expand-sm navbar-expand-lg fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="/">Rock Hard Excavating</a>
+                <a class="navbar-brand d-none d-md-block" href="/">Rock Hard Excavating</a>
 
                 {{--<ul class="nav navbar-nav ms-auto">--}}
                     {{--<li class="nav-item ms-4">--}}
