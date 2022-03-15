83faad1ffc9b47489b59620753fcc5fadae57bc5
vagrant <zachary.sluder@gmail.com>
2022-03-15 23:10:24 +0000

    changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="content-language" content="en-us">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="Rock Hard Excavating LLC provides excavating, asphalt and striping services all over Ohio.">
 
         @yield('meta')
 
