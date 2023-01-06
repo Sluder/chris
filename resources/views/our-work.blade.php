9664e0dbfc829b206138a772f9447ff8e51f1af1
Zachary Sluder <zachary.sluder@gmail.com>
2023-01-06 12:55:59 -0500

    text

--- resources/views/our-work.blade.php
+++ resources/views/our-work.blade.php
@@ -26,9 +26,13 @@
         <section>
             <h2>Attic</h2>
             <div class="divider"></div>
+
+            <h3><i>Before</i></h3>
             <img src="/img/our-work/attic/attic-1.jpg" style="width: 200px">
             <img src="/img/our-work/attic/attic-2.jpg" style="width: 200px">
             <img src="/img/our-work/attic/attic-3.jpg" style="width: 200px">
+
+            <h3 style="margin-top: 15px"><i>After</i></h3>
             <img src="/img/our-work/attic/attic-4.jpg" style="width: 200px">
             <img src="/img/our-work/attic/attic-5.jpg" style="width: 200px">
             <img src="/img/our-work/attic/attic-6.jpg" style="width: 200px">
@@ -41,6 +45,7 @@
             <img src="/img/our-work/kitchen/kitchen-2.jpeg" style="width: 200px">
             <img src="/img/our-work/kitchen/kitchen-3.jpeg" style="width: 200px">
             <img src="/img/our-work/kitchen/kitchen-4.jpeg" style="width: 200px">
+            <img src="/img/our-work/our-work-6.jpg" style="width: 200px">
         </section>
 
         <section>
@@ -50,8 +55,6 @@
             <img src="/img/our-work/our-work-2.jpg" style="width: 200px">
             <img src="/img/our-work/our-work-3.jpg" style="width: 200px">
             <img src="/img/our-work/our-work-4.jpg" style="width: 200px">
-            <img src="/img/our-work/our-work-5.jpg" style="width: 200px">
-            <img src="/img/our-work/our-work-6.jpg" style="width: 200px">
             <img src="/img/our-work/our-work-7.jpg" style="width: 200px">
             <img src="/img/our-work/our-work-8.jpg" style="width: 200px">
             <img src="/img/our-work/our-work-9.jpg" style="width: 200px">
