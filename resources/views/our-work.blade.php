04c974ebc85020549fc17af563d983a1a3b10ba3
Zachary Sluder <zachary.sluder@gmail.com>
2023-01-06 12:52:15 -0500

    text

--- resources/views/our-work.blade.php
+++ resources/views/our-work.blade.php
@@ -10,52 +10,52 @@
             <h2>Bathroom</h2>
             <div class="divider"></div>
 
-            <h3>Before</h3>
-            <img src="/img/our-work/bathroom/before/before-1.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/bathroom/before/before-2.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/bathroom/before/before-3.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/bathroom/before/before-4.jpg" style="height: 266.6px;border-radius: 15px">
-            <img src="/img/our-work/bathroom/before/before-5.jpg" style="height: 266.6px;border-radius: 15px">
+            <h3><i>Before</i></h3>
+            <img src="/img/our-work/bathroom/before/before-1.jpg" style="width: 200px">
+            <img src="/img/our-work/bathroom/before/before-2.jpg" style="width: 200px">
+            <img src="/img/our-work/bathroom/before/before-3.jpg" style="width: 200px">
+            <img src="/img/our-work/bathroom/before/before-4.jpg" style="height: 266.6px">
+            <img src="/img/our-work/bathroom/before/before-5.jpg" style="height: 266.6px">
 
-            <h3 style="margin-top: 15px">After</h3>
-            <img src="/img/our-work/bathroom/after-1.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/bathroom/after-2.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/bathroom/after-3.jpg" style="width: 200px;border-radius: 15px">
+            <h3 style="margin-top: 15px"><i>After</i></h3>
+            <img src="/img/our-work/bathroom/after-1.jpg" style="width: 200px">
+            <img src="/img/our-work/bathroom/after-2.jpg" style="width: 200px">
+            <img src="/img/our-work/bathroom/after-3.jpg" style="width: 200px">
         </section>
 
         <section>
             <h2>Attic</h2>
             <div class="divider"></div>
-            <img src="/img/our-work/attic/attic-1.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/attic/attic-2.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/attic/attic-3.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/attic/attic-4.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/attic/attic-5.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/attic/attic-6.jpg" style="width: 200px;border-radius: 15px">
+            <img src="/img/our-work/attic/attic-1.jpg" style="width: 200px">
+            <img src="/img/our-work/attic/attic-2.jpg" style="width: 200px">
+            <img src="/img/our-work/attic/attic-3.jpg" style="width: 200px">
+            <img src="/img/our-work/attic/attic-4.jpg" style="width: 200px">
+            <img src="/img/our-work/attic/attic-5.jpg" style="width: 200px">
+            <img src="/img/our-work/attic/attic-6.jpg" style="width: 200px">
         </section>
 
         <section>
             <h2>Kitchen</h2>
             <div class="divider"></div>
-            <img src="/img/our-work/kitchen/kitchen-1.jpeg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/kitchen/kitchen-2.jpeg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/kitchen/kitchen-3.jpeg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/kitchen/kitchen-4.jpeg" style="width: 200px;border-radius: 15px">
+            <img src="/img/our-work/kitchen/kitchen-1.jpeg" style="width: 200px">
+            <img src="/img/our-work/kitchen/kitchen-2.jpeg" style="width: 200px">
+            <img src="/img/our-work/kitchen/kitchen-3.jpeg" style="width: 200px">
+            <img src="/img/our-work/kitchen/kitchen-4.jpeg" style="width: 200px">
         </section>
 
         <section>
             <h2>Miscellaneous</h2>
             <div class="divider"></div>
-            <img src="/img/our-work/our-work-1.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-2.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-3.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-4.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-5.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-6.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-7.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-8.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-9.jpg" style="width: 200px;border-radius: 15px">
-            <img src="/img/our-work/our-work-10.jpg" style="width: 200px;border-radius: 15px">
+            <img src="/img/our-work/our-work-1.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-2.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-3.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-4.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-5.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-6.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-7.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-8.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-9.jpg" style="width: 200px">
+            <img src="/img/our-work/our-work-10.jpg" style="width: 200px">
         </section>
 
     </div>
